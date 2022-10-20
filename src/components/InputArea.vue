12f973a732d5aa3bc06a26fe3189c825274240a2
辣橙 <68316902+laorange@users.noreply.github.com>
2022-10-20 10:17:41 +0800

    perf: 输入框可直接清空

--- src/components/InputArea.vue
+++ src/components/InputArea.vue
@@ -14,6 +14,7 @@ const valueLocal = computed<string>({
   <n-form-item label="输入员工信息">
     <n-input
         v-model:value="valueLocal"
+        :clearable="true"
         type="textarea"
         placeholder="输入员工信息，每行一个员工"
         :autosize="true"
